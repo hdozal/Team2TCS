39302ad59f2391d295c4fc8738175330c8b2a793
Aravind Sankar <aravinds1996@gmail.com>
2021-06-28 13:43:42 -0400

    Added navbar and routing files

--- MediaApp/src/app/app.module.ts
+++ MediaApp/src/app/app.module.ts
@@ -3,13 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomenavbarComponent } from './components/homenavbar/homenavbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SportsComponent } from './components/sports/sports.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomenavbarComponent,
+    FooterComponent,
+    SportsComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
+    NgbModule,
     AppRoutingModule
   ],
   providers: [],
