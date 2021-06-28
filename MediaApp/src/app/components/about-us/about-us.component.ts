eeebc2aa04a547198530951bc74671979afc02bb
Aravind Sankar <aravinds1996@gmail.com>
2021-06-28 17:31:20 -0400

    Embedded maps into AboutUs Page

--- MediaApp/src/app/components/about-us/about-us.component.ts
+++ MediaApp/src/app/components/about-us/about-us.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+declare const L: any;
+
+
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
@@ -9,8 +12,63 @@ export class AboutUsComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-    console.log("i clicked on about us")
+  ngOnInit() {
+    if (!navigator.geolocation) {
+      console.log('location is not supported');
+    }
+    navigator.geolocation.getCurrentPosition((position) => {
+      const coords = position.coords;
+      const latLong = [coords.latitude, coords.longitude];
+      console.log(
+        `lat: ${position.coords.latitude}, lon: ${position.coords.longitude}`
+      );
+      let mymap = L.map('map').setView(latLong, 13);
+
+      L.tileLayer(
+        'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYXJhdmluZHMxOTk2IiwiYSI6ImNrcWg0ZjBrMzE0YWUycHBlcm16OW1lNjcifQ.rTmLnOC7rexlaLeKtuGHgg',
+        {
+          attribution:
+            'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+          maxZoom: 18,
+          id: 'mapbox/streets-v11',
+          tileSize: 512,
+          zoomOffset: -1,
+          accessToken: 'your.mapbox.access.token',
+        }
+      ).addTo(mymap);
+
+      let marker = L.marker(latLong).addTo(mymap);
+
+      marker.bindPopup('<b>Hi</b>').openPopup();
+
+      let popup = L.popup()
+        .setLatLng(latLong)
+        .setContent('We are here')
+        .openOn(mymap);
+    });
+    this.watchPosition();
   }
 
+  watchPosition() {
+    let desLat = 0;
+    let desLon = 0;
+    let id = navigator.geolocation.watchPosition(
+      (position) => {
+        console.log(
+          `lat: ${position.coords.latitude}, lon: ${position.coords.longitude}`
+        );
+        if (position.coords.latitude === desLat) {
+          navigator.geolocation.clearWatch(id);
+        }
+      },
+      (err) => {
+        console.log(err);
+      },
+      {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+      }
+    );
+  }
 }
