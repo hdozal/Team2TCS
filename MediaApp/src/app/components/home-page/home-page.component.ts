103946c7b73a68388c7fdb900b155e55ec42687e
Aravind Sankar <aravinds1996@gmail.com>
2021-06-28 16:12:12 -0400

    Added carousel,footer to HomePage

--- MediaApp/src/app/components/home-page/home-page.component.ts
+++ MediaApp/src/app/components/home-page/home-page.component.ts
@@ -4,13 +4,17 @@ import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: ['./home-page.component.css'],
+  providers: [NgbCarouselConfig],
+  // styles: [` .img-fluid{ min-width:100%}`]
+
 })
 export class HomePageComponent implements OnInit {
   images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  
   constructor(config: NgbCarouselConfig) {
     // customize default values of carousels used by this component tree
-    config.interval = 10000;
+    config.interval = 5000;
     config.wrap = false;
     config.keyboard = false;
     config.pauseOnHover = false;
